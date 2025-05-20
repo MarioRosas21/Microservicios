ab203090e0241858675266643a83cd6e584ccbd9
LENOVO <LENOVO@DESKTOP-IN5HT21>
2025-05-20 01:05:45 -0600

    Variables agregadas

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'metro.proxy.rlwy.net',
-  user: 'root',
-  password: 'hYtgMQukWtJlnUpKJNfuihUCYkIKsHbk',
-  database: 'railway',
+  host: process.env.DB_HOST || 'metro.proxy.rlwy.net',
+  port: process.env.DB_PORT || '15016',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || 'hYtgMQukWtJlnUpKJNfuihUCYkIKsHbk',
+  database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
